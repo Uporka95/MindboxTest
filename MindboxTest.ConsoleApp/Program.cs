1b0164e364e9bfd6e4218e0d31f28dced7566cfd
Uporka95 <maxitronix197@gmail.com>
2024-03-24 18:22:29 +0300

    #1

--- MindboxTest.ConsoleApp/Program.cs
+++ MindboxTest.ConsoleApp/Program.cs
@@ -11,10 +11,8 @@ namespace MindboxTest.ConsoleApp
            figures.Add(new Circle(16));
            figures.Add(new Rectangle(5, 5));
            figures.Add(new Triangle(0,5,6));
-           Triangle triangle = new(3, 5, 4);
 
-           bool result = triangle.IsRight;
-            foreach (var figure in figures)
+           foreach (var figure in figures)
            {
                 Console.WriteLine("Площадь фигуры " + (figures.IndexOf(figure) + 1) + " : " + figure.Square);
            }
